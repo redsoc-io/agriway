2f79f5979c41ca91b72573bf0396175d1e8d4885
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-05-29 04:40:35 +0530

    Add sup_points and border table, simulate AI

--- src/components/views/Home/SupervisorHome.js
+++ src/components/views/Home/SupervisorHome.js
@@ -1,6 +1,7 @@
 import react from "react";
 import { useEffect, useState } from "react";
 
+
 export default function SupervisorHome() {
     const [points, setPoints] = useState(0);
     useEffect(() => {
@@ -18,6 +19,37 @@ export default function SupervisorHome() {
             <div className="flex items-center justify-start py-4">
                 <div className="rounded-md shadow px-3 py-4 border">
                     Redeemable Points: {points}
+                </div>
+            </div>
+            <div>
+                <div className="rounded-md shadow px-3 py-4 border w-full py-4">
+                    <h2>TODOs</h2>
+                    <table className="w-full my-5">
+                        <thead>
+                            <tr>
+                                <th>Task</th>
+                                <th>Field</th>
+                                <th>Before</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>plant potatoes</td>
+                                <td>kumaragiri farm</td>
+                                <td>13-06-2022</td>
+                            </tr>
+                            <tr>
+                                <td>plant carrot</td>
+                                <td>kaveri</td>
+                                <td>11-06-2022</td>
+                            </tr>
+                            <tr>
+                                <td>plant cabbage</td>
+                                <td>mysore</td>
+                                <td>15-06-2022</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
--- src/pages/api/sup_points.js
+++ src/pages/api/sup_points.js
@@ -1,12 +1,16 @@
 import db from "../../lib/mysql"
 import { format } from "sqlstring"
+import { getSession } from "next-auth/react";
 
 export default async function (req, res) {
 
+    const session = await getSession({ req });
     var status = [];
     try {
+        const supid = JSON.parse(session.token.sub).id
         const query = format(
-            "SELECT DISTINCT COUNT(*) from users, inventory, traders where users.id = traders.sup_id and traders.id = inventory.trader_id and users.id = 1;"
+            "SELECT DISTINCT COUNT(*) from users, inventory, traders where users.id = traders.sup_id and traders.id = inventory.trader_id and users.id = ?;",
+            [supid]
         );
         status = await db.query(query);
         status = status[0]["COUNT(*)"] * 5;
